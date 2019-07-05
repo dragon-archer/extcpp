07a7fb9c81624079275a74c4486c9f6bc4e031b0
dragon-archer <dragon-archer@outlook.com>
2019-07-05 18:03:32 +0800

    Add basic files for libextcpp.dll

---
+++ src/dllmain.cpp
@@ -0,0 +1,24 @@
+﻿/*
+*********************************************************
+* @file       dllmain.cpp
+* @brief      The main entry of libextcpp.dll
+* @author     dragon-archer <dragon-archer@outlook.com>
+* @since      v0.1
+* @license    The MIT License
+*             Copyright (C) 2019 dragon-archer
+*********************************************************
+*/
+#include "pch.h"
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	switch(ul_reason_for_call)
+	{
+		case DLL_PROCESS_ATTACH:
+		case DLL_THREAD_ATTACH:
+		case DLL_THREAD_DETACH:
+		case DLL_PROCESS_DETACH:
+			break;
+	}
+	return TRUE;
+}
---
+++ src/pch.h
@@ -0,0 +1,18 @@
+﻿/*
+*********************************************************
+* @file       pch.h
+* @brief      The precompiled header
+* @author     dragon-archer <dragon-archer@outlook.com>
+* @since      v0.1
+* @license    The MIT License
+*             Copyright (C) 2019 dragon-archer
+*********************************************************
+*/
+#ifndef _LIBEXTCPP_PCH_H
+#define _LIBEXTCPP_PCH_H
+
+// Windows Header
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#endif // _LIBEXTCPP_PCH_H
