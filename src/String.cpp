f0c5d5c84e2108e8bb5f715250f65173e7debeb1
dragon-archer <dragon-archer@outlook.com>
2019-09-08 09:04:20 +0800

    Update dllmain,cpp
    Remove unused headers

---
+++ src/String.cpp
@@ -0,0 +1,11 @@
+/*
+*********************************************************
+* @file       String.cpp
+* @brief      The precompiled header
+* @author     dragon-archer <dragon-archer@outlook.com>
+* @since      v0.1
+* @license    The MIT License
+*             Copyright (C) 2019 dragon-archer
+*********************************************************
+*/
+#include <pch.h>
